def698a5bcd7e7e345c8bac5a6e4161d6a9cbc57
nraymond <nraymond@excilys.com>
2016-04-19 15:29:37 +0200

    feat(DAO) : Add DAO abstract class

---
+++ ComputerDatabase/src/persistence/DAO.java
@@ -0,0 +1,60 @@
+package persistence;
+
+import java.sql.SQLException;
+
+import javax.sql.RowSet;
+
+/**
+ * Permets to acces to the database to control the mapping
+ * of a table as an entity
+ * @param <T> The type of the entity that represents a table
+ */
+public abstract class DAO<T> implements AutoCloseable {
+	
+	protected RowSet rowset = null;
+	
+	public DAO() throws SQLException {
+		this.rowset = Database.getFreshRowSet();
+	}
+	
+	@Override
+	public void close() throws Exception {
+		this.rowset.close();
+	}
+	
+
+	/* DAO Specification */
+	
+	/**
+	 * Finds the entity that match the given id.
+	 * @param id The entity's id
+	 * @return The required entity
+	 */
+	public abstract T find(long id) throws SQLException;
+	
+	/**
+	 * Remove the entity that match the given id from
+	 * the database and returns it.
+	 * @param id The entity's id
+	 * @return The entity that has been removed
+	 */
+	public abstract T remove(long id) throws SQLException;
+	
+	/**
+	 * Add the given entity in the database.
+	 * @param entity The entity to add in the database
+	 */
+	public abstract void add(T entity) throws SQLException;
+	
+	/**
+	 * Find the entity that match the given id and update it
+	 * with the update value.
+	 * @param id The entity's id
+	 * @param updateValue The new value that will have the desired
+	 * entity.
+	 */
+	public abstract boolean update(long id, T updateValue) throws SQLException;
+
+	
+}
+
