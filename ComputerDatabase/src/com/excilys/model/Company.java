13a58cd39f283b079e27e26c7835b4bf392773d7
nraymond <nraymond@excilys.com>
2016-04-21 18:01:20 +0200

    <v1.1> [Entities] Feat : Add hashcode and equals functions

--- ComputerDatabase/src/com/excilys/model/Company.java
+++ ComputerDatabase/src/com/excilys/model/Company.java
@@ -6,10 +6,16 @@ public class Company {
 
 	String name = null;
 
+	
+	/* Constructors */
+	
 	public Company(long id) {
 		this.id = id;
 	}
 
+	
+	/* Getters and Setters */
+	
 	public long getId() {
 		return id;
 	}
@@ -26,9 +32,42 @@ public class Company {
 		this.name = name;
 	}
 
+	
+	/* Object */
+	
 	@Override
 	public String toString() {
 		return "(" + id + ", " + name + ")";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
 	}
 
 }
