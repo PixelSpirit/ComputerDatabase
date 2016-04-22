fcc43511e5fd3d4b5afde6b753c1b8b6265ae249
nraymond <nraymond@excilys.com>
2016-04-22 14:15:38 +0200

    [ConnectionFactory] Refactor : Now throw a ConnectionException

--- ComputerDatabase/src/com/excilys/persistence/ConnectionFactory.java
+++ ComputerDatabase/src/com/excilys/persistence/ConnectionFactory.java
@@ -4,6 +4,10 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
 /**
  * Gives an access to the computer database
  */
@@ -13,12 +17,20 @@ public class ConnectionFactory {
 	private static final String PASSWORD = "qwerty1234";
 	private static final String URL = "jdbc:mysql://localhost:3306/computer-database-db";
 	
+	private static Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
+	
 	/**
 	 * Returns a fresh connection to the database
 	 * @return a fresh connection to the database
+	 * @throws ConnectionException 
 	 * @throws SQLException if the database has encounter an error
 	 */
-	public static Connection get() throws SQLException {
-		return DriverManager.getConnection(URL, USER, PASSWORD);
+	public static Connection get() throws ConnectionException {
+		try {
+			return DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			logger.error("<SQLException> Can't connect to database !");
+			throw new ConnectionException(e);
+		}
 	}
 }
