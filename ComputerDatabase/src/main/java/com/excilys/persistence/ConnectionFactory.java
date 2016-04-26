d4a1397e561bd5fbafacf35daee9d9e51b2c99b2
nraymond <nraymond@excilys.com>
2016-04-26 12:17:29 +0200

    [ConnectionFactory] Feat : Now will use test database if in test

--- ComputerDatabase/src/main/java/com/excilys/persistence/ConnectionFactory.java
+++ ComputerDatabase/src/main/java/com/excilys/persistence/ConnectionFactory.java
@@ -1,6 +1,5 @@
 package com.excilys.persistence;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,6 +14,8 @@ import org.slf4j.LoggerFactory;
  */
 public class ConnectionFactory {
 
+    private static String propertyFile = "db.properties";
+
     private static Logger logger = LoggerFactory.getLogger(ConnectionFactory.class);
 
     /**
@@ -27,7 +28,8 @@ public class ConnectionFactory {
     public static Connection get() throws ConnectionException {
         try {
             Properties props = new Properties();
-            props.load(new FileInputStream("src/main/resources/db.properties"));
+
+            props.load(ConnectionFactory.class.getClassLoader().getResourceAsStream(propertyFile));
             Class.forName(props.getProperty("DB_DRIVER"));
             return DriverManager.getConnection(props.getProperty("DB_URL"), props.getProperty("DB_USERNAME"),
                     props.getProperty("DB_PASSWORD"));
