29f2b734c6d419dfd13c5c1e72df58cfc5185f82
nraymond <nraymond@excilys.com>
2016-04-26 17:34:51 +0200

    [Servlets] Feature : Add ComputerServlet

---
+++ ComputerDatabase/src/main/webapp/com/excilys/controlers/ComputersServlet.java
@@ -0,0 +1,28 @@
+package com.excilys.controlers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ComputersServlet
+ */
+@WebServlet("/ComputersServlet")
+public class ComputersServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/views/computers.jsp").forward(request, response);
+    }
+
+}
