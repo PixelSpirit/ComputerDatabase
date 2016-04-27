254ebe4d502729afa4c1e78f0a12d7d5002a225d
nraymond <nraymond@excilys.com>
2016-04-27 17:03:05 +0200

    [DTO] Feature : Creates DTOs

---
+++ ComputerDatabase/src/main/java/com/excilys/model/DTOCompany.java
@@ -0,0 +1,33 @@
+package com.excilys.model;
+
+public class DTOCompany {
+
+    private long id;
+    private String name;
+
+    /**
+     * Constructs a DTOCompany.
+     * @param id The id
+     * @param name The name
+     */
+    public DTOCompany(long id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+}
---
+++ ComputerDatabase/src/main/java/com/excilys/model/DTOComputer.java
@@ -0,0 +1,74 @@
+package com.excilys.model;
+
+public class DTOComputer {
+
+    private long id;
+    private String name;
+    private String introduced;
+    private String discontinued;
+    private String companyId;
+    private String companyName;
+
+    /**
+     * Constructs a DTOComputer.
+     * @param id The id
+     * @param name The name
+     * @param introduced The introduced date
+     * @param discontinued The discontinued date
+     * @param companyId The company id
+     * @param companyName The company name
+     */
+    public DTOComputer(long id, String name, String introduced, String discontinued, String companyId,
+            String companyName) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.introduced = introduced;
+        this.discontinued = discontinued;
+        this.companyId = companyId;
+        this.companyName = companyName;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return the introduced
+     */
+    public String getIntroduced() {
+        return introduced;
+    }
+
+    /**
+     * @return the discontinued
+     */
+    public String getDiscontinued() {
+        return discontinued;
+    }
+
+    /**
+     * @return the companyId
+     */
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    /**
+     * @return the companyName
+     */
+    public String getCompanyName() {
+        return companyName;
+    }
+
+}
