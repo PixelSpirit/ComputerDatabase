9a36df0aefb26fbf116ed46a93b3864eba624b20
nraymond <nraymond@excilys.com>
2016-04-28 13:37:09 +0200

    [AddComputerServlet] Feature : Now save the companies

--- ComputerDatabase/src/main/webapp/com/excilys/controlers/AddComputerServlet.java
+++ ComputerDatabase/src/main/webapp/com/excilys/controlers/AddComputerServlet.java
@@ -7,21 +7,39 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.excilys.model.Company;
+import com.excilys.persistence.CompanyDAO;
+import com.excilys.service.ServiceException;
+import com.excilys.service.SimpleServices;
+
 /**
  * Servlet implementation class AddComputerServlet
  */
 public class AddComputerServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
-     *      response)
-     */
+    private SimpleServices<Company> companiesService = new SimpleServices<>(CompanyDAO.getInstance());
+
+    private Logger logger = LoggerFactory.getLogger(AddComputerServlet.class);
+
+    private void saveAllCompanies(HttpServletRequest request) throws ServiceException {
+        request.setAttribute("allCompanies", companiesService.findAll());
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        response.getWriter().append("Served at: ").append(request.getContextPath());
+        try {
+            saveAllCompanies(request);
+            this.getServletContext().getRequestDispatcher("/views/addComputer/addComputer.jsp").forward(request,
+                    response);
+        } catch (ServiceException e) {
+            logger.error("[Catch] <" + e.getClass().getSimpleName() + "> " + e.getMessage());
+            this.getServletContext().getRequestDispatcher("/views/404.html").forward(request, response);
+        }
     }
 
 }
